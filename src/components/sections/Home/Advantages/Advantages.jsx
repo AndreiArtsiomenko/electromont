a8073856b6c20770b45cd20bd6c0879b71b1676f
AndreiArtsiomenko <andartsiomenko@gmail.com>
2026-01-02 04:02:50 +0300

    add style

--- src/components/sections/Home/Advantages/Advantages.jsx
+++ src/components/sections/Home/Advantages/Advantages.jsx
@@ -1,28 +1,63 @@
+import {
+  ShieldCheck,
+  Wrench,
+  ClipboardCheck,
+  Clock,
+  Sparkles,
+  Award,
+  Wallet
+} from 'lucide-react';
+
 import Container from '../../../ui/Container/Container';
 import styles from './Advantages.module.css';
 
 const advantages = [
-  'Используем только проверенные материалы',
-  'Применяем профессиональный инструмент',
-  'Соблюдаем все нормы и правила',
-  'Четкие сроки выполнения работ',
-  'Чистота после монтажа',
-  'Гарантия 2 года на все работы',
-  'Учитываем ваши пожелания и бюджет'
+  {
+    icon: ShieldCheck,
+    text: 'Используем только проверенные материалы'
+  },
+  {
+    icon: Wrench,
+    text: 'Применяем профессиональный инструмент'
+  },
+  {
+    icon: ClipboardCheck,
+    text: 'Соблюдаем все нормы и правила'
+  },
+  {
+    icon: Clock,
+    text: 'Четкие сроки выполнения работ'
+  },
+  {
+    icon: Sparkles,
+    text: 'Чистота после монтажа'
+  },
+  {
+    icon: Award,
+    text: 'Гарантия 2 года на все работы'
+  },
+  {
+    icon: Wallet,
+    text: 'Учитываем ваши пожелания и бюджет'
+  }
 ];
 
 const Advantages = () => {
   return (
-    <section className={styles.advantages}>
+    <section className={styles.section}>
       <Container>
-        <h2>Наши преимущества</h2>
+        <h2 className={styles.title}>Наши преимущества</h2>
+
         <ul className={styles.list}>
-          {advantages.map((text, i) => (
-            <li key={i} className={styles.item}>
-              <span className={styles.icon}>✔</span>
-              {text}
-            </li>
-          ))}
+          {advantages.map((item, i) => {
+            const Icon = item.icon;
+            return (
+              <li key={i} className={styles.item}>
+                <Icon className={styles.icon} />
+                <span>{item.text}</span>
+              </li>
+            );
+          })}
         </ul>
       </Container>
     </section>
